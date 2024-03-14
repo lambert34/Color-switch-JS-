d782a408f19fd653a4ea052814901021071d20a7
alexL <aleksandrlambert@gmail.com>
2024-03-14 16:37:17 +0700

    добавляет массив цветов и функцию для случ.выбора

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 const button = document.getElementById("btn");
 
+const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71", "#ff5733", "f9ff33", "#f1f5f8", "red", "green", "blue"];
+
 button.addEventListener("click", () => {
     console.log("click");
+    console.log(getRandomNumber());
 })
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
