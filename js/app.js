63516be4cbb1429f824f51f6c83b9bcd909434df
alexL <aleksandrlambert@gmail.com>
2024-03-14 16:41:55 +0700

    вешаем на body функцию random
    теперь цвет меняется, но есть повторы, когда одно значение массива случайно выбирается дважды подряд

--- js/app.js
+++ js/app.js
@@ -3,8 +3,7 @@ const button = document.getElementById("btn");
 const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71", "#ff5733", "f9ff33", "#f1f5f8", "red", "green", "blue"];
 
 button.addEventListener("click", () => {
-    console.log("click");
-    console.log(getRandomNumber());
+    document.body.style.backgroundColor = colors[getRandomNumber()];
 })
 
 function getRandomNumber() {
